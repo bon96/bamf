efaf0a0968cf24840a353b25cf4e250868ec69b9
Tommi <tommi.karppa@gmail.com>
2019-10-04 02:55:48 +0300

    getters for putfield and putstatic

--- src/main/java/org/bon/jvm/instructions/Putfield.java
+++ src/main/java/org/bon/jvm/instructions/Putfield.java
@@ -1,6 +1,8 @@
 package org.bon.jvm.instructions;
 
+import org.bon.api.util.Type;
 import org.bon.jvm.constantpool.ConstPool;
+import org.bon.jvm.constantpool.constants.FieldRefConstant;
 
 import java.io.DataInputStream;
 import java.io.IOException;
@@ -13,10 +15,32 @@ import java.io.IOException;
 
 public class Putfield extends Instruction {
 
+    private ConstPool constPool;
     private int index;
 
     public Putfield(int index) {
         this.index = index;
+    }
+
+    public Putfield(ConstPool constPool, int index) {
+        this.constPool = constPool;
+        this.index = index;
+    }
+
+    public FieldRefConstant getFieldRef() {
+        return constPool.get(index).cast();
+    }
+
+    public String getTargetClass() {
+        return getFieldRef().getConstClass().getName();
+    }
+
+    public String getTarget() {
+        return getFieldRef().getNameAndType().getName();
+    }
+
+    public Type getType() {
+        return new Type(getFieldRef().getNameAndType().getDescriptor());
     }
 
     @Override
@@ -25,6 +49,6 @@ public class Putfield extends Instruction {
     }
 
     public static Instruction from(DataInputStream in, ConstPool constPool) throws IOException {
-        return new Putfield(in.readUnsignedShort());
+        return new Putfield(constPool, in.readUnsignedShort());
     }
 }
